8d72374c282d7738d67066f4670ed5f6432b2214
Nathanael Shermett <nathanael@shermett.me>
2021-11-30 16:24:52 -0500

    Sort patterns by specificity. Prevent double linkification.

--- src/LinkifyTracking.php
+++ src/LinkifyTracking.php
@@ -10,33 +10,33 @@ class LinkifyTracking
     protected $args;
 
     /**
-     * @var array The tracking carriers.
+     * @var array The tracking carriers, sorted by pattern specificity (descending).
      */
     protected $carriers = [
-        [
-            'label' => 'DHL',
-            'url' => 'http://www.dhl.com/content/g0/en/express/tracking.shtml?brand=DHL&AWB=%s',
-            'regex' => '/\b(\d{4}[- ]?\d{4}[- ]?\d{2}|\d{3}[- ]?\d{8}|[A-Z]{3}\d{7})\b/i'
-        ],
+		[
+			'label' => 'USPS',
+			'url' => 'https://tools.usps.com/go/TrackConfirmAction?tLabels=%s',
+			'regex' => [
+				'/\b((420 ?\d{5} ?)?(91|92|93|94|01|03|04|70|23|13)\d{2} ?\d{4} ?\d{4} ?\d{4} ?\d{4}( ?\d{2,6})?)\b/i',
+				'/\b((M|P[A-Z]?|D[C-Z]|LK|E[A-C]|V[A-Z]|R[A-Z]|CP|CJ|LC|LJ) ?\d{3} ?\d{3} ?\d{3} ?[A-Z]?[A-Z]?)\b/i',
+				'/\b(82 ?\d{3} ?\d{3} ?\d{2})\b/i'
+			]
+		],
+		[
+			'label' => 'UPS',
+			'url' => 'http://wwwapps.ups.com/WebTracking/processInputRequest?TypeOfInquiryNumber=T&InquiryNumber1=%s',
+			'regex' => '/\b(1Z ?[0-9A-Z]{3} ?[0-9A-Z]{3} ?[0-9A-Z]{2} ?[0-9A-Z]{4} ?[0-9A-Z]{3} ?[0-9A-Z]|T\d{3} ?\d{4} ?\d{3})\b/i'
+		],
         [
             'label' => 'FedEx',
             'url' => 'https://www.fedex.com/apps/fedextrack/?action=track&locale=en_US&cntry_code=us&tracknumbers=%s',
             'regex' => '/\b(((96\d\d|6\d)\d{3} ?\d{4}|96\d{2}|\d{4}) ?\d{4} ?\d{4}( ?\d{3})?)\b/i'
         ],
-        [
-            'label' => 'UPS',
-            'url' => 'http://wwwapps.ups.com/WebTracking/processInputRequest?TypeOfInquiryNumber=T&InquiryNumber1=%s',
-            'regex' => '/\b(1Z ?[0-9A-Z]{3} ?[0-9A-Z]{3} ?[0-9A-Z]{2} ?[0-9A-Z]{4} ?[0-9A-Z]{3} ?[0-9A-Z]|T\d{3} ?\d{4} ?\d{3})\b/i'
-        ],
-        [
-            'label' => 'USPS',
-            'url' => 'https://tools.usps.com/go/TrackConfirmAction?tLabels=%s',
-            'regex' => [
-                '/\b((420 ?\d{5} ?)?(91|92|93|94|01|03|04|70|23|13)\d{2} ?\d{4} ?\d{4} ?\d{4} ?\d{4}( ?\d{2,6})?)\b/i',
-                '/\b((M|P[A-Z]?|D[C-Z]|LK|E[A-C]|V[A-Z]|R[A-Z]|CP|CJ|LC|LJ) ?\d{3} ?\d{3} ?\d{3} ?[A-Z]?[A-Z]?)\b/i',
-                '/\b(82 ?\d{3} ?\d{3} ?\d{2})\b/i'
-            ]
-        ],
+		[
+			'label' => 'DHL',
+			'url' => 'http://www.dhl.com/content/g0/en/express/tracking.shtml?brand=DHL&AWB=%s',
+			'regex' => '/\b(\d{4}[- ]?\d{4}[- ]?\d{2}|\d{3}[- ]?\d{8}|[A-Z]{3}\d{7})\b/i'
+		],
         [
             'label' => 'Royal Mail',
             'url' => 'http://www.royalmail.com/portal/rm/track?trackNumber=%s',
@@ -108,19 +108,21 @@ class LinkifyTracking
      */
     public function linkify(string $content)
     {
+    	$linkficationQueue = [];
         foreach ($this->carriers as $carrier) {
             foreach ((array) $carrier['regex'] as $regex) {
                 $content = preg_replace_callback(
                     $regex,
-                    function ($matches) use ($carrier) {
-                        return $this->generateHtmlLink($carrier, $matches[0]);
+                    function ($matches) use (&$linkficationQueue, $carrier) {
+						$linkficationQueue[] = $this->generateHtmlLink($carrier, $matches[0]);
+                        return '%s';
                     },
                     $content
                 );
             }
         }
 
-        return $content;
+		return sprintf($content, ...$linkficationQueue);
     }
 
     /**
